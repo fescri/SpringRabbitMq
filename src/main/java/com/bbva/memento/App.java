c61d1b24e2a6e9f83fe75bdc7b11eb2f1b96dace
Manuel Trejo Rico <manuel.trejo.rico.contractor@bbva.com>
2015-12-23 11:49:35 +0100

    Write connection to an exchange and a queue established. Read connection to a queue established.

--- src/main/java/com/bbva/memento/App.java
+++ src/main/java/com/bbva/memento/App.java
@@ -1,6 +1,7 @@
 package com.bbva.memento;
 
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,10 +9,18 @@ public class App  {
 
     public static void main(String[] args) throws InterruptedException {
         ApplicationContext context = new AnnotationConfigApplicationContext(RabbitConfiguration.class);
-        AmqpTemplate template = context.getBean(AmqpTemplate.class);
 
-        template.convertAndSend("myqueue", "foo");
+        //AmqpTemplate template = context.getBean(AmqpTemplate.class);
 
-        String foo = (String) template.receiveAndConvert("myqueue");
+        RabbitTemplate template = context.getBean(RabbitTemplate.class);
+
+        template.setExchange(context.getBean("exchange", String.class));
+        template.setRoutingKey(context.getBean("routingKey", String.class));
+
+        template.convertAndSend("mensaje enviado");
+
+        String message = (String) template.receiveAndConvert(context.getBean("queue", String.class));
+
+        System.out.println("MESSAGE RECEIVED: " + message);
     }
 }
--- src/main/java/com/bbva/memento/RabbitConfiguration.java
+++ src/main/java/com/bbva/memento/RabbitConfiguration.java
@@ -1,10 +1,7 @@
 package com.bbva.memento;
 
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,22 +11,37 @@ public class RabbitConfiguration {
 
     @Bean
     public ConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("192.168.99.100");
+        connectionFactory.setPort(5001);
+        //connectionFactory.setUsername("guest");
+        //connectionFactory.setPassword("guest");
+
         return connectionFactory;
     }
-
+/*
     @Bean
     public AmqpAdmin amqpAdmin() {
         return new RabbitAdmin(connectionFactory());
     }
-
+*/
     @Bean
     public RabbitTemplate rabbitTemplate() {
         return new RabbitTemplate(connectionFactory());
     }
 
-    @Bean
-    public Queue myQueue() {
-        return new Queue("myqueue");
+    @Bean(name = "exchange")
+    public String exchange() {
+        return ("intercambio");
+    }
+
+    @Bean(name = "routingKey")
+    public String routingKey() {
+        return ("clave");
+    }
+
+    @Bean(name = "queue")
+    public String queue() {
+        return ("exampleManu");
     }
 }
